97f37b2993aace7397cc5894ca39396a2d772c4c
Nishajangir0972 <nishajangir9302@gmail.com>
2024-02-21 17:59:51 -0800

    Adding vercel file

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ const app = express()
 
 const connection = mongoose.connect(process.env.MONGO_URL)
 app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://tagore-clg-three.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+
 app.use('/uploads', express.static('uploads'))
 app.use(express.json())
 app.use("/data" , StudentRouter)
