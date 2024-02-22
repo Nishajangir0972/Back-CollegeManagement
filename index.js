51e7132124e856370d085e919aee7a7c8d82b53c
Nishajangir0972 <nishajangir9302@gmail.com>
2024-02-21 17:39:33 -0800

    update files

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express()
 const connection = mongoose.connect(process.env.MONGO_URL)
 app.use(cors())
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://tagore-g7j5aj4r5-nishajangir0972.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', 'https://tagore-clg-three.vercel.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', true);
